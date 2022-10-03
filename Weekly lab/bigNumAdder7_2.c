27a6231ab7f4f7c8afd81f38e051daa7fa96512d
Schmaew <109778280+Schmaew@users.noreply.github.com>
2022-10-03 23:51:06 +0700

    Add files via upload

---
+++ Weekly lab/bigNumAdder7_2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+void main()
+
+{
+
+long int a;
+
+long int b;
+
+printf("Enter a long integer a :");
+
+scanf("%ld",&a);
+
+printf("Enter a long integer b :");
+
+scanf("%ld",&b);
+
+printf("%ld",a+b);
+
+}
